4282a7c79106bae92423107d2f57d796ca3c2b06
rednaxela700 <ohleg@windowslive.com>
2020-06-27 23:01:50 +0200

    fixed mobile menu scroll issue

--- js/main.js
+++ js/main.js
@@ -10,11 +10,13 @@ const navBtn = document.querySelector('.js-navBtn')
 createBtnAnimation(navBtn)
 
 function hamburger() {
+    const body = document.querySelector('body')
     const navWrapper = document.querySelector('.nav__wrapper')
     const navContainer = navWrapper.querySelector('.nav__container')
     const navIcon = document.querySelector('.hamburger__btn')
 
     navIcon.addEventListener('click', () => {
+        body.classList.toggle('overflow--hidden')
         navContainer.classList.toggle('mobile--hidden')
         navWrapper.classList.toggle('nav__wrapper--show')
     })
