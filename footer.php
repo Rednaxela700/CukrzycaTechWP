877c921180fec36d78ea93d87cc5fe9bd416cad1
rednaxela700 <ohleg@windowslive.com>
2020-06-27 23:01:19 +0200

    added page wrapper

--- footer.php
+++ footer.php
@@ -1,22 +1,25 @@
-<?php wp_footer(); ?>
 <footer class="footer">
     <div class="footer__container">
         <div class="footer__item">
-            <img src="<?php echo get_template_directory_uri() ?>/img/cukrzyca_techfest_footer.png" alt="footer logo" class="footer__logo">
+            <img src="<?php echo get_template_directory_uri() ?>/img/cukrzyca_techfest_footer.png" alt="footer logo"
+                 class="footer__logo">
             <div class="social__container">
                 <div class="social__item">
                     <a href="https://" target="_blank" referrerpolicy="no-referrer" rel="noopener">
-                        <img src="<?php echo get_template_directory_uri() ?>/img/social_fb.svg" alt="socialmedia_icon" class="social__img">
+                        <img src="<?php echo get_template_directory_uri() ?>/img/social_fb.svg" alt="socialmedia_icon"
+                             class="social__img">
                     </a>
                 </div>
                 <div class="social__item">
                     <a href="https://" target="_blank" referrerpolicy="no-referrer" rel="noopener">
-                        <img src="<?php echo get_template_directory_uri() ?>/img/social_tw.svg" alt="socialmedia_icon" class="social__img">
+                        <img src="<?php echo get_template_directory_uri() ?>/img/social_tw.svg" alt="socialmedia_icon"
+                             class="social__img">
                     </a>
                 </div>
                 <div class="social__item">
                     <a href="https://" target="_blank" referrerpolicy="no-referrer" rel="noopener">
-                        <img src="<?php echo get_template_directory_uri() ?>/img/social_li.svg" alt="socialmedia_icon" class="social__img">
+                        <img src="<?php echo get_template_directory_uri() ?>/img/social_li.svg" alt="socialmedia_icon"
+                             class="social__img">
                     </a>
                 </div>
             </div>
@@ -24,7 +27,8 @@
         <div class="footer__item"><span>Projekt graficzny: Cary One</span></div>
     </div>
 </footer>
-
+</div>
+<?php wp_footer(); ?>
 <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
 <script>
     window.ga = function () {
--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade
     your browser</a> to improve your experience and security.</p>
 <![endif]-->
-
+<div class="page__wrapper">
 <header class="header">
     <div class="header__wrapper">
         <div class="header__container">
