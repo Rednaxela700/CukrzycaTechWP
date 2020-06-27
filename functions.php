1922d4753d5ebf39c04cf2f365acd210a06c299a
rednaxela700 <ohleg@windowslive.com>
2020-06-28 00:36:10 +0200

    removed index.css from functions

--- functions.php
+++ functions.php
@@ -3,7 +3,6 @@
 ini_set('display_errors', 'all');
 wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
 wp_enqueue_style('normalize.css', get_template_directory_uri() . '/css/normalize.css');
-wp_enqueue_style('index.css', get_template_directory_uri() . '/css/index.css');
 wp_enqueue_style('main.css', get_template_directory_uri() . '/css/main.css', false, '1.1', 'all');
 wp_enqueue_style('fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css', false, '1.1', 'all');
 wp_enqueue_script('modernizr', get_template_directory_uri() . '/js/vendor/modernizr-3.8.0.min.js');
