61f77eb6ba54475e1935a86224e886f4be7f7bad
rednaxela700 <ohleg@windowslive.com>
2020-06-28 04:06:21 +0200

    hardcoded nav btn animation in js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // btn animations in gsap
 function createBtnAnimation(btn) {
     const btnWidth = btn.offsetWidth
-    btn.addEventListener('mouseover', () => gsap.to('.js-navBtn', {duration: .3, width: btnWidth + 20}))
+    btn.addEventListener('mouseover', () => gsap.to('.js-navBtn', {duration: .3, width: 222 + 20 }))
     btn.addEventListener('mouseout', () => gsap.to('.js-navBtn', {duration: .3, width: btnWidth}))
 
 }
 
 const navBtn = document.querySelector('.js-navBtn')
-createBtnAnimation(navBtn)
+    createBtnAnimation(navBtn)
 
 function hamburger() {
     const body = document.querySelector('body')
