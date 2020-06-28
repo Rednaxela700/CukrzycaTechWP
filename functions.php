810faf10b4cb7c24d4f5e784c71a58d00986c03b
rednaxela700 <ohleg@windowslive.com>
2020-06-28 03:57:00 +0200

    fixed file paths in css, updated gsap

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ wp_enqueue_style('normalize.css', get_template_directory_uri() . '/css/normalize
 wp_enqueue_style('main.css', get_template_directory_uri() . '/css/main.css', false, '1.1', 'all');
 wp_enqueue_style('fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css', false, '1.1', 'all');
 wp_enqueue_script('modernizr', get_template_directory_uri() . '/js/vendor/modernizr-3.8.0.min.js');
-wp_enqueue_script('gsap', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.2.4/gsap.min.js', [], 1, true);
+wp_enqueue_script('gsap', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.3.4/gsap.min.js', [], 1, true);
 wp_enqueue_script('plugins', get_template_directory_uri() . '/js/plugins.js', 'main', 1, true);
 wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js', 'gsap', 1, true);
 wp_enqueue_script('lazy', get_template_directory_uri() . '/js/lazy.js', [], 1, true);
